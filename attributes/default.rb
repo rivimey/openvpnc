002b7477cf7e0a8a6f24284fb6eed8bc61d74135
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2013-11-19 15:00:34 +0000

    initial

---
+++ attributes/default.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: openvpn
+# Attributes:: openvpn
+#
+# Copyright 2009-2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['openvpn']['remote']          = "unset"
+default['openvpn']['proto']           = 'udp'
+default['openvpn']['port']            = '1194'
+default['openvpn']['type']            = 'client'
+default['openvpn']['log']             = '/var/log/openvpn.log'
+
+default["openvpn"]["ns_cert_type"]    = "server"
+default["openvpn"]["tls_cipher"]      = "DHE-RSA-AES256-SHA"
+default["openvpn"]["cipher"]          = "AES-256-CBC"
+
+default['openvpn']['key_dir']         = '/etc/openvpn/keys'
+default['openvpn']['signing_ca_key']  = "#{node["openvpn"]["key_dir"]}/ca.key"
+default['openvpn']['signing_ca_cert'] = "#{node["openvpn"]["key_dir"]}/ca.crt"
+default['openvpn']['routes']          = []
+default['openvpn']['user']            = 'nobody'
+default['openvpn']['group']           = case node['platform_family']
+                                        when 'rhel'
+                                          'nobody'
+                                        else
+                                          'nogroup'
+                                        end
---
+++ recipes/client.rb
@@ -0,0 +1,58 @@
+#
+# Cookbook Name:: openvpn
+# Recipe:: default
+#
+# Copyright 2009-2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+key_dir  = node['openvpn']['key_dir']
+
+package 'openvpn' do
+  action :install
+end
+
+directory key_dir do
+  owner 'root'
+  group 'root'
+  mode  '0700'
+end
+
+ovpnclient 'client' do
+  port node['openvpn']['port']
+  proto node['openvpn']['proto']
+  type node['openvpn']['type']
+  local node['openvpn']['local']
+  remote node['openvpn']['remote']
+  mssfix node['openvpn']['mssfix']
+  fragment node['openvpn']['fragment']
+  ns_cert_type node['openvpn']['ns_cert_type']
+  tls_cipher node['openvpn']['tls_cipher']
+  cipher node['openvpn']['cipher']
+  routes node['openvpn']['routes']
+  script_security node['openvpn']['script_security']
+  key_dir node['openvpn']['key_dir']
+  key_size node['openvpn']['key']['size']
+  subnet node['openvpn']['subnet']
+  netmask node['openvpn']['netmask']
+  user node['openvpn']['user']
+  group node['openvpn']['group']
+  log node['openvpn']['log']
+
+  notifies :restart, 'service[openvpn]'
+end
+
+service 'openvpn' do
+  action [:enable, :start]
+end
